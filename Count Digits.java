2afbd7055f32db929b931a83fe82650800f36de0
Abhishek Choudhary <abhishek.choudhary2242@gmail.com>
2024-03-17 20:22:09 +0530

    #

--- Count Digits.java
+++ Count Digits.java
@@ -1,11 +1,10 @@
-
-    public static int countDigits(int n){
-       int k = n;
-       int count = 0;
-       while(k>0){
-           int ld = k%10;
-           if(ld!=0 && n%ld==0) count++;
-            k/=10;
-       }
-       return count;
+public static int countDigits(int n){
+    int k = n;
+    int count = 0;
+    while(k>0){
+        int ld = k%10;
+        if(ld!=0 && n%ld==0) count++;
+         k/=10;
     }
+    return count;
+ }
